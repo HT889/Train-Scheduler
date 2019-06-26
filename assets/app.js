5cccf830cda2cf076368546952407f4b6453b6aa
Haig Talarian <haigtalarian@gmail.com>
2019-06-26 10:40:09 -0700

    another lame initial commit, html tweaks, logic soon

---
+++ assets/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDhqJNDFHrS5PRXjG42XqCmp9CNbFP6uZk",
+    authDomain: "train-time-3cdb0.firebaseapp.com",
+    databaseURL: "https://train-time-3cdb0.firebaseio.com",
+    projectId: "train-time-3cdb0",
+    storageBucket: "train-time-3cdb0.appspot.com",
+    messagingSenderId: "242982667279",
+    appId: "1:242982667279:web:9c169fd68623a268"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
