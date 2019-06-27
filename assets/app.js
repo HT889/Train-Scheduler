6aa171da73f8a1fe1072a19fae5db5e9e4f77008
Haig Talarian <haigtalarian@gmail.com>
2019-06-26 22:23:54 -0700

    almost done, still getting NaN for minutes until train when page is refreshed

--- assets/app.js
+++ assets/app.js
@@ -7,5 +7,55 @@ var firebaseConfig = {
     messagingSenderId: "242982667279",
     appId: "1:242982667279:web:9c169fd68623a268"
   };
-  // Initialize Firebase
+  // initialize firebase
   firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+  $(document).ready(function() {
+    //starting values:
+    var trainName = "";
+    var destination = "";
+    var firstTrainTime = "";
+    var frequency = "";
+    // console.log('moment',moment.js);
+
+    $("#submit").on("click", function() {
+      trainName = $("#train-name").val().trim();
+      destination = $("#destination").val().trim();
+      firstTrainTime = $("#first-train-time").val().trim();
+      frequency = $("#frequency").val().trim();
+
+      database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        firstTrainTime: firstTrainTime,
+        frequency: frequency
+      })
+
+      $("#train-name").val("");
+      $("#destination").val("");
+      $("#first-train-time").val("");
+      $("#frequency").val("");
+
+      // console.log(trainName, destination, firstTrainTime, frequency)
+
+    })
+    database.ref().on("child_added", function(snapshot){
+      var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+      var currentTime = moment();
+      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+      var tRemainder = diffTime % snapshot.val().frequency;
+      var tMinutesTillTrain = snapshot.val().frequency - tRemainder;
+      var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
+
+
+
+
+      // console.log(snapshot.val())
+      $("#main-table").append($("<tr>").append($("<td>").text(snapshot.val().trainName), $("<td>").text(snapshot.val().destination), $("<td>").text(snapshot.val().frequency), $("<td>").text(nextTrain), $("<td>").text(tMinutesTillTrain)))
+    })
+    // console.log(tMinutesTillTrain)
+
+  })
